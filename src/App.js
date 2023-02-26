fbb5978207f8458fbac0bb12e25f79bea9472cf9
Pratham Shah <prathamshah1006@gmail.com>
2023-02-26 23:39:08 +0530

    Gave some styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
+import Footer from "./components/Footer/Footer";
+import Header from './components/Header/Header';
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <BrowserRouter>
+    <div className="app">
+      <Header/>
+      <Routes>
+        <Route path="/" element= {<Home/>} exact/>
+        <Route path="/result" element= {<Result/>} exact/>
+        <Route path="/quiz" element= {<Quiz/>} exact/>
+      </Routes>
     </div>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Quiz = () => {
+    
+  return (
+    <div>
+      Quiz page
+    </div>
+  )
+}
+
+export default Quiz
