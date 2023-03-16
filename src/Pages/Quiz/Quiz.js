4e5048438fd6fa4cd32fad7616b5c539598193d4
Pratham Shah <prathamshah1006@gmail.com>
2023-03-17 01:42:59 +0530

    quiz functionality added

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -6,13 +6,14 @@ import "./Quiz.css"
 const Quiz = () => {
   
   let url = "https://quizapi.io/api/v1/questions?apiKey=QZVXFNuqJYR6x8DGAM2JDc2vIIiQUjOWIujIBFRh&&limit=10"
-  const [myData, setMyData] = useState([]);
+  const [myData, setMyData] = useState();
   // const myData = [];
   const [name, setName] = useState(localStorage.getItem("name"));
   const [difficulty, setDifficulty] = useState(localStorage.getItem("difficulty"));
   const [category, setCategory] = useState(localStorage.getItem("category"));
   const [score, setScore] = useState(0);
   const [index, setIndex] = useState(0);
+  const [selected, setSelected] = useState();
   const getApiData = async () => {
     const res = await axios.get(
       `https://quizapi.io/api/v1/questions?apiKey=QZVXFNuqJYR6x8DGAM2JDc2vIIiQUjOWIujIBFRh&&limit=10&&category=${category}&&difficulty=${difficulty}`
@@ -22,10 +23,23 @@ const Quiz = () => {
   
   useEffect(() => {
     getApiData();
-  }, []);
+  }, [1]);
 
   console.log(myData);
 
+  const handleNext = (e)=>{
+    e.preventDefault();
+    setIndex(index+1)
+  }
+  const handlePrevious = (e)=>{
+    e.preventDefault();
+    setIndex(index-1)
+  }
+  const handleClick = (button) =>{
+    if (button === myData[index].correct_answer) {
+      setScore(score+1)
+    }
+  }
   // axios.get(url)  
   //     .then(res => {  
   //       const questions = res.data;  
@@ -49,41 +63,91 @@ const Quiz = () => {
       <h1>Question {index + 1} :</h1>
 
       <div className="singleQuestion">
-        <h2>db mofmbpovieqvnievneiwfnf</h2>
+        <h2>{myData[index].question}</h2>
         <div className="options">
           {/* {error && <ErrorMessage>{error}</ErrorMessage>} */}
-          {/* {options &&
-            options.map((i) => (
+          {/* {myData[index].answers &&
+            myData[index].answers.map((i) => (
               <button
-                className={`singleOption  ${selected && handleSelect(i)}`}
+                // className={`singleOption  
+                // ${selected && handleSelect(i)}`
+                // }
                 key={i}
                 // onClick={() => handleCheck(i)}
-                disabled={selected}
+                // disabled={selected}
               >
                 {i}
-              </button> */}
-            {/* ))} */}
+              </button>
+           ))} */}
+          {
+            myData[index].answers.answer_a ? (
+              <button
+              className={selected==="answer_a"?("singleOption selected"):("singleOption")}
+              // onClick = {handleClick("answer_a")}
+              >
+                {myData[index].answers.answer_a}
+              </button>
+            ):null
+          }
+          {
+            myData[index].answers.answer_b ? (
+              <button
+              className='singleOption'>
+                {myData[index].answers.answer_b}
+              </button>
+            ):null
+          }
+          {
+            myData[index].answers.answer_c ? (
+              <button
+              className='singleOption'>
+                {myData[index].answers.answer_c}
+              </button>
+            ):null
+          }
+          {
+            myData[index].answers.answer_d ? (
+              <button
+              className='singleOption'>
+                {myData[index].answers.answer_d}
+              </button>
+            ):null
+          }
+          {
+            myData[index].answers.answer_e ? (
+              <button
+              className='singleOption'>
+                {myData[index].answers.answer_e}
+              </button>
+            ):null
+          }
+          {
+            myData[index].answers.answer_f ? (
+              <button
+              className='singleOption'>
+                {myData[index].answers.answer_f}
+              </button>
+            ):null
+          }
         </div>
         <div className="controls">
           <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            style={{ width: 185 }}
-            href="/"
-            // onClick={() => handleQuit()}
+            variant='contained'
+            color='primary'
+            size='large'
+            style={{width:185}}
+            onClick={handlePrevious}
           >
-            Quit
+            Previous
           </Button>
           <Button
             variant="contained"
             color="primary"
             size="large"
             style={{ width: 185 }}
-            // onClick={handleNext}
+            onClick={handleNext}
           >
-            Submit
-            {/* {currQues > 20 ? "Submit" : "Next Question"} */}
+            {index>10 ? "Submit" : "Next"}
           </Button>
         </div>
       </div>
